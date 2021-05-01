0225d6e7c2a4b6af814708ee80843b8d0e8b8a81
user <user@WINDOWS-VK4C0HF>
2021-05-01 13:51:15 +0200

    Implement API for Comments.

---
+++ src/main/java/com/example/springredditclone/controller/CommentsController.java
@@ -0,0 +1,33 @@
+package com.example.springredditclone.controller;
+
+import com.example.springredditclone.dto.CommentsDto;
+import com.example.springredditclone.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+@AllArgsConstructor
+public class CommentsController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/by-post/{postId}")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@PathVariable Long postId) {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getAllCommentsForPost(postId));
+    }
+
+    @GetMapping("/by-user/{userName}")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@PathVariable String userName) {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getAllCommentsForUser(userName));
+    }
+}
