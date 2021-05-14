64bc21a47a7c5749eaf427eb498b37e3d7940117
user <user@WINDOWS-VK4C0HF>
2021-05-14 22:46:20 +0200

    Rewrite sample tests using AssertJ.

--- src/test/java/com/example/springredditclone/service/CommentServiceTest.java
+++ src/test/java/com/example/springredditclone/service/CommentServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CommentServiceTest {
@@ -13,16 +15,23 @@ public class CommentServiceTest {
     @DisplayName("Test should pass when comment does not contain swear words")
     void shouldNotContainSwearWordsInsideComment() {
         CommentService commentService = new CommentService(null, null, null, null, null, null, null);
-        Assertions.assertFalse(commentService.containsSwearWords("This is a clean comment"));
+//        Assertions.assertFalse(commentService.containsSwearWords("This is a clean comment"));
+
+        assertThat(commentService.containsSwearWords("This is a comment")).isFalse();
     }
 
     @Test
     @DisplayName("Test should throw exception when comment contains swear words")
     public void shouldThrowExceptionWhenCommentContainsSwearWords() {
         CommentService commentService = new CommentService(null, null, null, null, null, null, null);
-        SpringRedditException exception = assertThrows(SpringRedditException.class, () -> {
+//        SpringRedditException exception = assertThrows(SpringRedditException.class, () -> {
+//            commentService.containsSwearWords("This is shitty comment!");
+//        });
+//        assertTrue(exception.getMessage().contains("Comment contains unacceptable language!"));
+
+        assertThatThrownBy(() -> {
             commentService.containsSwearWords("This is shitty comment!");
-        });
-        assertTrue(exception.getMessage().contains("Comment contains unacceptable language!"));
+        }).isInstanceOf(SpringRedditException.class)
+                .hasMessage("Comment contains unacceptable language!");
     }
 }
