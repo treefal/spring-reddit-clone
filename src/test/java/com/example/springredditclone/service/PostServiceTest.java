a7cecef6071cbe92f2f97c4d4390d0ca1b9b2b27
user <user@WINDOWS-VK4C0HF>
2021-05-15 00:09:53 +0200

    Refactor and test void method with Mockito's verify and ArgumentCaptor.

--- src/test/java/com/example/springredditclone/service/PostServiceTest.java
+++ src/test/java/com/example/springredditclone/service/PostServiceTest.java
@@ -1,33 +1,53 @@
 package com.example.springredditclone.service;
 
+import com.example.springredditclone.dto.PostRequest;
 import com.example.springredditclone.dto.PostResponse;
 import com.example.springredditclone.mapper.PostMapper;
 import com.example.springredditclone.model.Post;
+import com.example.springredditclone.model.Subreddit;
+import com.example.springredditclone.model.User;
 import com.example.springredditclone.repository.PostRepository;
 import com.example.springredditclone.repository.SubredditRepository;
 import com.example.springredditclone.repository.UserRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
+import java.util.Collections;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
 
-    private SubredditRepository subredditRepository = Mockito.mock(SubredditRepository.class);
-    private PostRepository postRepository = Mockito.mock(PostRepository.class);
-    private UserRepository userRepository = Mockito.mock(UserRepository.class);
-    private AuthService authService = Mockito.mock(AuthService.class);
-    private PostMapper postMapper = Mockito.mock(PostMapper.class);
+    @Mock
+    private SubredditRepository subredditRepository;
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private AuthService authService;
+    @Mock
+    private PostMapper postMapper;
+
+    @Captor
+    private ArgumentCaptor<Post> postArgumentCaptor;
+
+    private PostService postService;
+
+    @BeforeEach
+    public void setup() {
+        postService = new PostService(subredditRepository, postRepository, userRepository, authService, postMapper);
+    }
 
     @Test
     @DisplayName("Should find post by id")
     public void shouldFindPostById() {
-        PostService postService = new PostService(subredditRepository, postRepository, userRepository, authService, postMapper);
         Post post = new Post(123L, "First Post", "http://url.site", "Test",
                 0, null, Instant.now(), null);
         PostResponse expectedPostResponse = new PostResponse(123L, "First Post",
@@ -42,5 +62,33 @@ public class PostServiceTest {
 
         Assertions.assertThat(actualPostResponse.getId()).isEqualTo(expectedPostResponse.getId());
         Assertions.assertThat(actualPostResponse.getPostName()).isEqualTo(expectedPostResponse.getPostName());
+    }
+
+    @Test
+    @DisplayName("Should save post")
+    public void shouldSavePost() {
+        User currentUser = new User(123L, "test user", "secret password",
+                "user@gmail.com", Instant.now(), true);
+        Subreddit subreddit = new Subreddit(123L, "First Subreddit", "Subreddit Description",
+                Collections.emptyList(), Instant.now(), currentUser);
+        Post post = new Post(123L, "First Post", "http://url.site", "Text",
+                0, null, Instant.now(), null);
+        PostRequest postRequest = new PostRequest(null, "First Subreddit", "First Post",
+                "http://url.site", "Text");
+
+        Mockito.when(subredditRepository.findByName("First Subreddit"))
+                .thenReturn(Optional.of(subreddit));
+        Mockito.when(authService.getCurrentUser())
+                .thenReturn(currentUser);
+        Mockito.when(postMapper.map(postRequest, subreddit, currentUser))
+                .thenReturn(post);
+
+        postService.save(postRequest);
+
+        // Use ArgumentCaptor to capture elements passed in as arguments void methods
+        Mockito.verify(postRepository, Mockito.times(1)).save(postArgumentCaptor.capture());
+
+        Assertions.assertThat(postArgumentCaptor.getValue().getPostId()).isEqualTo(123L);
+        Assertions.assertThat(postArgumentCaptor.getValue().getPostName()).isEqualTo("First Post");
     }
 }
